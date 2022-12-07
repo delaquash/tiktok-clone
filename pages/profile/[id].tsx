62bf526ba544cdaa6336326ddf4f097553ff60e6
delaquash <olaide1191@gmail.com>
2022-12-07 16:51:50 +0100

    fix: profile route

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import axios, { Axios } from 'axios';
+import Image from 'next/image';
+import React, {useEffect, useState } from 'react';
+import { GoVerified } from 'react-icons/go';
+import NoResults from '../components/NoResults';
+import VideoCard from '../components/VideoCard';
+import { IUser, Video } from '../../types';
+import { BASE_URL } from '../../utils';
+
 
 const profile = () => {
   return (
     <div>profile</div>
   )
+}
+
+export const getServerSideProps =async ({params:id}: { params: {id: string}}) => {
+        const res = await axios.get(`${BASE_URL}/profile/${id}`)
+        return {
+          props: {
+            data: res.data
+          }
+        }
 }
 
 export default profile
